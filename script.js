9bdf73a50ed3cbf35da04b272231f2ba2583bfbb
Tai Odunaiya <tai.odunaiya@yahoo.com>
2024-07-17 15:59:31 -0500

    fixes

--- script.js
+++ script.js
@@ -385,29 +385,30 @@ document.querySelectorAll(".service-btn").forEach((service) => {
 
 // Section 5
 // Form
-const formHeading = document.querySelector('.form-heading');
-const formInputs = document.querySelectorAll('.contact-form-input');
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
 
-formInputs.forEach(input => {
-    input.addEventListener('focus', () => {
+formInputs.forEach((input) => {
+    input.addEventListener("focus", () => {
         formHeading.style.opacity = "0";
         setTimeout(() => {
             formHeading.textContent = `Your ${input.placeholder}`;
-            formHeading.style.opacity = "1"
+            formHeading.style.opacity = "1";
         }, 300);
     });
-    input.addEventListener('blur', () => {
+
+    input.addEventListener("blur", () => {
         formHeading.style.opacity = "0";
         setTimeout(() => {
-            formHeading.textContent = `Let's Talk`;
-            formHeading.style.opacity = "1"
+            formHeading.textContent = "Let's Talk";
+            formHeading.style.opacity = "1";
         }, 300);
     });
 });
 // End of Form
 
 // Slideshow
-const slideshow = document.querySelector('.slideshow');
+const slideshow = document.querySelector(".slideshow");
 
 setInterval(() => {
     const firstIcon = slideshow.firstElementChild;
@@ -416,9 +417,9 @@ setInterval(() => {
 
     const thirdIcon = slideshow.children[3];
 
-    thirdIcon.classList.add('light');
+    thirdIcon.classList.add("light");
 
-    thirdIcon.previousElementSibling.classList.remove('light');
+    thirdIcon.previousElementSibling.classList.remove("light");
 
     setTimeout(() => {
         slideshow.removeChild(firstIcon);
@@ -427,10 +428,10 @@ setInterval(() => {
 
         setTimeout(() => {
             firstIcon.classList.remove("faded-out");
-        }, 500)
+        }, 500);
     }, 500);
-}, 3000)
-// End of SlideShow
+}, 3000);
+// End of Slideshow
 
 // Form Validation
 const form = document.querySelector(".contact-form");
@@ -441,55 +442,53 @@ const message = document.getElementById("message");
 const messages = document.querySelectorAll(".message");
 
 const error = (input, message) => {
-  input.nextElementSibling.classList.add("error");
-  input.nextElementSibling.textContent = message;
+    input.nextElementSibling.classList.add("error");
+    input.nextElementSibling.textContent = message;
 };
 
 const success = (input) => {
-  input.nextElementSibling.classList.remove("error");
+    input.nextElementSibling.classList.remove("error");
 };
 
 const checkRequiredFields = (inputArr) => {
-  inputArr.forEach((input) => {
-    if (input.value.trim() === "") {
-      error(input, `${input.id} is required`);
-    }
-  });
+    inputArr.forEach((input) => {
+        if (input.value.trim() === "") {
+            error(input, `${input.id} is required`);
+        }
+    });
 };
 
 const checkLength = (input, min) => {
-  if (input.value.trim().length < min) {
-    error(input, `${input.id} must be at least ${min} characters`);
-  } else {
-    success(input);
-  }
+    if (input.value.trim().length < min) {
+        error(input, `${input.id} must be at least ${min} characters`);
+    } else {
+        success(input);
+    }
 };
 
 const checkEmail = (input) => {
-  const regEx =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regEx =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (regEx.test(input.value.trim())) {
-    success(input);
-  } else {
-    error(input, "Email is not valid");
-  }
+    if (regEx.test(input.value.trim())) {
+        success(input);
+    } else {
+        error(input, "Email is not valid");
+    }
 };
 
 form.addEventListener("submit", (e) => {
-  checkLength(username, 2);
-  checkLength(subject, 2);
-  checkLength(message, 10);
-  checkEmail(email);
-  checkRequiredFields([username, email, subject, message]);
+    checkLength(username, 2);
+    checkLength(subject, 2);
+    checkLength(message, 10);
+    checkEmail(email);
+    checkRequiredFields([username, email, subject, message]);
 
-  const notValid = Array.from(messages).find((message) => {
-    return message.classList.contains("error");
-  });
+    const notValid = Array.from(messages).find((message) => {
+        return message.classList.contains("error");
+    });
 
-  notValid && e.preventDefault();
+    notValid && e.preventDefault();
 });
-
-
 // End of Form Validation
-// End Of Section 5
+// End of Section 5
